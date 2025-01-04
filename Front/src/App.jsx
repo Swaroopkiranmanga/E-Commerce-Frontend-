00fcab4a8c3768530f08fa78d2dfc69664c006d7
Swaroop Kiran <swaroopkiranmanga@gmail.com>
2025-01-04 12:15:38 +0530

    "Added AdminDashboard"

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -1,37 +1,29 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-import NavBar from './components/NavBar.jsx'; 
-import Pics from './components/Pics.jsx';
-import Carouse from './components/Carouse.jsx';
-import Top from './components/Top.jsx';
-import Top2 from './components/Top2.jsx';
-import Top3 from './components/Top3.jsx';
-import Login from './components/Login.jsx'; 
-import Register from './components/Register.jsx';
-import ProductsPage from './components/ProductsPage.jsx';
-import ProductItem from './components/ProductItem.jsx';
-
-
- 
+import NavBar from "./components/NavBar.jsx";
+import Pics from "./components/Pics.jsx";
+import Carouse from "./components/Carouse.jsx";
+import Top from "./components/Top.jsx";
+import Top2 from "./components/Top2.jsx";
+import Top3 from "./components/Top3.jsx";
+import Login from "./components/Login.jsx";
+import Register from "./components/Register.jsx";
+import ProductsPage from "./components/ProductsPage.jsx";
+import ProductItem from "./components/ProductItem.jsx";
+import AdminDashboard from "./components/AdminDashboard.jsx";
 
 function App() {
   return (
-    
     <Router>
-     
       <NavBar />
 
-     
       <Routes>
         <Route
           path="/"
           element={
             <>
-              <Pics >
-               
-              </Pics>
+              <Pics></Pics>
               <Carouse />
               <Top />
               <Top2 />
@@ -39,20 +31,18 @@ function App() {
             </>
           }
         />
-          <Route path="/products/:id" element={<ProductsPage />} />
-     
-        <Route path="/login" element={<Login />} >
-        </Route>
-        <Route path="/register" element={<Register />} >
-       
-        </Route>
-        <Route path="/productitem" element={<ProductItem></ProductItem>}></Route>
-      </Routes>
+        <Route path="/products/:id" element={<ProductsPage />} />
 
-      
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/register" element={<Register />}></Route>
+        <Route
+          path="/productitem"
+          element={<ProductItem></ProductItem>}
+        ></Route>
+        <Route path="/AdminDashboard" element={<AdminDashboard />} />
+      </Routes>
     </Router>
   );
 }
 
 export default App;
-
